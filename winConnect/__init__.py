cc2d807d81d74f3249ff50ff385a6100845368ba
SantaSpeen <santaspeen@yandex.ru>
2025-03-12 15:36:38 +0300

    [~] Separate meta info to __meta__.py

--- winConnect/__init__.py
+++ winConnect/__init__.py
@@ -1,14 +1,4 @@
-# -*- coding: utf-8 -*-
-
-__title__ = 'winConnect'
-__description__ = 'Communicate Client-Server via Windows NamedPipe.'
-__url__ = 'https://github.com/SantaSpeen/winConnect'
-__version__ = '0.9.1'
-__build__ = 16
-__author__ = 'SantaSpeen'
-__author_email__ = 'admin@anidev.ru'
-__license__ = "MIT"
-__copyright__ = 'Copyright 2025 © SantaSpeen'
-
 from .WinConnectDaemon import WinConnectDaemon
 from .WinConnectClient import WinConnectClient
+
+from .__meta__ import *
