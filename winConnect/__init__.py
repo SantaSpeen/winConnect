2040824e1e7d3cc61d7ab55ffcee37e3531e51f7
SantaSpeen <santaspeen@yandex.ru>
2025-03-12 15:31:09 +0300

    [+] Update __description__

--- winConnect/__init__.py
+++ winConnect/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'winConnect'
-__description__ = 'Communication Client->Daemon via NamedPipe.'
+__description__ = 'Communicate Client-Server via Windows NamedPipe.'
 __url__ = 'https://github.com/SantaSpeen/winConnect'
-__version__ = '0.8.1'
+__version__ = '0.9.1'
 __build__ = 16
 __author__ = 'SantaSpeen'
 __author_email__ = 'admin@anidev.ru'
