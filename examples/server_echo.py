9f57667bd0e25b274d6adae153f1b67cb8bbe88c
SantaSpeen <santaspeen@yandex.ru>
2025-03-13 11:38:11 +0300

    rename

--- examples/server_echo.py
+++ examples/server_echo.py
@@ -3,9 +3,8 @@ from winConnect import WinConnectDaemon
 connector = WinConnectDaemon('test')
 # Set header settings
 # see: https://docs.python.org/3.13/library/struct.html#format-characters
-# Default: ">L"
-# >L - Big-endian long integer (header_size: 4 bytes, max_size: 4294967295)
-connector.set_header_settings(">L")
+# Default: ">H" - Big-endian unsigned short integer (header_size: 2 bytes, max_size: 65535)
+connector.set_header_settings(">H")
 
 for data in connector.listen():
     print(f"({type(data)}) {data=}")
