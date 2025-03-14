ea520cd8e4ba868539f2f315f33431b18c8365c3
SantaSpeen <santaspeen@gmail.com>
2025-03-15 02:39:07 +0300

    remove examples

--- examples/server_echo.py
+++
@@ -1,9 +0,0 @@
-from winConnect import WinConnectDaemon
-
-connector = WinConnectDaemon('test')
-
-for data in connector.listen():
-    print(f"({type(data)}) {data=}")
-    if data is None and connector.closed:
-        break
-    connector.send_data(data)
